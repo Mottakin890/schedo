c0963ecd649bdf4c2ba1ab241980bf00021903ab
Mottakin890 <mottakinulalam@gmail.com>
2026-04-29 04:34:40 +0100

    chore(drop): Previous featured droped.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.api_prep_dio"
+    namespace = "com.mottakin.schedo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.api_prep_dio"
+        applicationId = "com.mottakin.schedo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
-import 'package:api_prep_dio/common/resources/routes/app_pages.dart';
-import 'package:api_prep_dio/common/resources/routes/app_routes.dart';
-import 'package:api_prep_dio/common/utils/di/dependency_injection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-
-import 'common/resources/assets/app_colors.dart';
 
 void main(List<String> args) {
-  _init();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
     _,
   ) {
@@ -24,30 +17,17 @@ void main(List<String> args) {
   });
 }
 
-void _init() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarBrightness: Brightness.light,
-      statusBarColor: AppColors.cTransparent,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
-}
-
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialBinding: DependencyInjection(),
-      getPages: AppRoutes.routes,
-      initialRoute: AppPages.splash,
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       scrollBehavior: const MaterialScrollBehavior().copyWith(
         overscroll: false,
       ),
+      home: Scaffold(),
     );
   }
 }
